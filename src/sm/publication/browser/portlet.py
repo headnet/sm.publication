ed60a6daecb30885d31b51b881cea53bb187cbc2
Bo Simonsen <bo@geekworld.dk>
2013-10-28 15:45:13 +0100

    Further work on the publication module

---
+++ src/sm/publication/browser/portlet.py
@@ -0,0 +1,50 @@
+# encoding: utf-8
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+from collective.person import MessageFactory as _
+
+from plone.app.portlets.browser import z3cformhelper
+from plone.app.portlets.portlets import base
+from plone.portlets.interfaces import IPortletDataProvider
+
+from z3c.form import field
+
+from zope import schema
+from zope.interface import implements
+
+
+class IPublicationPortlet(IPortletDataProvider):
+
+    header = schema.TextLine(
+        title=_(u"Portlet header"),
+        description=_(u"Header of the rendered portlet"),
+        required=False)
+
+
+class Assignment(base.Assignment):
+    implements(IPublicationPortlet)
+
+    def __init__(self, **kwargs):
+        for (key, value) in kwargs.items():
+            setattr(self, key, value)
+
+    @property
+    def title(self):
+        return self.header
+
+
+class Renderer(base.Renderer):
+    render = ViewPageTemplateFile('publication_portlet.pt')
+
+
+class AddForm(z3cformhelper.AddForm):
+    fields = field.Fields(IPublicationPortlet)
+    label = _(u"Add Publication Portlet")
+
+    def create(self, data):
+        return Assignment(**data)
+
+
+class EditForm(z3cformhelper.EditForm):
+    fields = field.Fields(IPublicationPortlet)
+    label = _(u"Edit Publication portlet")
---
+++ src/sm/publication/content/publication.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+from zope.interface import Interface, implements
+from zope.component import adapts
+from plone.z3cform.fieldsets.extensible import FormExtender
+from plone.z3cform.fieldsets.interfaces import IFormExtender
+from plone.dexterity.content import Item
+
+from z3c.form.interfaces import IEditForm
+
+
+class IPublication(Interface):
+    """ Publication """
+
+
+class Publication(Item):
+    implements(IPublication)
+
+
+class EffectiveDateRequired(FormExtender):
+    implements(IFormExtender)
+    adapts(Interface, IDefaultBrowserLayer, IEditForm)
+
+    def __init__(self, context, request, form):
+        self.context = context
+        self.request = request
+        self.form = form
+
+    def update(self):
+        if self.context.portal_type == 'Publication':
+            for group in self.form.groups:
+                if 'IDublinCore.effective' in group.fields:
+                    field = group.fields['IDublinCore.effective'].field
+                    field.required = True
