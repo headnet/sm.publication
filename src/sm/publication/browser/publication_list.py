79207a455d90a59b4da8ece17cbcacc110e7cf99
Bo Simonsen <bo@geekworld.dk>
2013-11-07 16:28:35 +0100

    Improved publication list and search.

--- src/sm/publication/browser/publication_list.py
+++ src/sm/publication/browser/publication_list.py
@@ -2,25 +2,62 @@
 
 from copy import deepcopy
 from Acquisition import aq_inner
+from DateTime import DateTime
 
+from Products.CMFCore.utils import getToolByName
 from Products.Five.browser import BrowserView
 
 from plone.memoize.view import memoize
 from plone.app.querystring.querybuilder import QueryBuilder
 
+START_YEAR = 1990
 
-class PublicationListView(BrowserView):
-    """
-    """
+
+class PublicationListSearchView(BrowserView):
+    has_search = True
+
+    def subject_to_text(self, subjects):
+        results = []
+
+        if not subjects:
+            return []
+
+        for subject in subjects:
+            translation = self.context.translate(
+                subject,
+                domain='collective.taxonomy.publication_subjects'
+            )
+            results.append(translation)
+
+        return results
+
+    @property
+    @memoize
+    def all_years(self):
+        current_year = DateTime().year()
+        return range(START_YEAR, current_year + 1)
+
+    @property
+    @memoize
+    def all_subjects(self):
+        subjects = []
+
+        catalog_tool = getToolByName(self.context, 'portal_catalog')
+        index = catalog_tool._catalog.indexes['taxonomy_publication_subjects']
+
+        for key in index.uniqueValues():
+            subjects.append(key)
+
+        return zip(subjects, self.subject_to_text(subjects))
 
     @memoize
     def results(self, batch=True, b_start=0, b_size=None):
-        """
-        """
-        sort_on = 'portal_type'
+        sort_on = 'effective'
+        sort_order = 'reverse'
         context = aq_inner(self.context)
         query = context.getQuery()
-        searchable = self.request.get("SearchableText", "")
+
+        searchable = self.request.get("searchable", "")
         if searchable:
             query = deepcopy(query)
             row = {}
@@ -29,9 +66,26 @@ class PublicationListView(BrowserView):
             row['v'] = searchable
             query.append(row)
 
-        # sorry!
+        subject = self.request.get("subject", "")
+        if subject:
+            query = deepcopy(query)
+            row = {}
+            row['i'] = "taxonomy_publication_subjects"
+            row['o'] = "plone.app.querystring.operation.string.is"
+            row['v'] = subject
+            query.append(row)
+
+        year = self.request.get("year", "")
+        if year:
+            query = deepcopy(query)
+            row = {}
+            row['i'] = "effective"
+            row['o'] = "plone.app.querystring.operation.date.between"
+            row['v'] = [DateTime(int(year), 01, 01, 0, 0, 0),
+                        DateTime(int(year), 12, 31, 23, 59, 59)]
+            query.append(row)
+
         querybuilder = QueryBuilder(context, self.request)
-        sort_order = 'reverse' if context.sort_reversed else 'ascending'
 
         size = b_size or context.item_count or self.default_bsize
         results = querybuilder(
@@ -40,3 +94,7 @@ class PublicationListView(BrowserView):
             limit=context.limit
         )
         return results
+
+
+class PublicationListView(PublicationListSearchView):
+    has_search = False
