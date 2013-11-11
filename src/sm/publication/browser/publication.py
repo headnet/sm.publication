b41e530ec1394f45b34b0b564782a5a1790e6d3a
Bo Simonsen <bo@geekworld.dk>
2013-11-11 10:55:56 +0100

    Added GA tracking code

--- src/sm/publication/browser/publication.py
+++ src/sm/publication/browser/publication.py
@@ -9,7 +9,15 @@ class PublicationView(BrowserView):
         return scale.scale('image').tag(css_class='publication_image')
 
     @property
+    def relative_download_link(self):
+        relative_link = '/'.join(self.context.getPhysicalPath()[2:])
+        relative_link += '/@@download/publication'
+        return '/' + relative_link
+
+    @property
     def download_link(self):
-        return self.context.absolute_url() + '/@@download/publication'
+        download_link = self.context.absolute_url() + '/@@download/publication'
+        return download_link
+
 
 
